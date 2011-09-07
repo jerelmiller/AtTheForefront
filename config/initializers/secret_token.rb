348160756483d648c2bead7f2a18819df162598e
Jerel Miller <jermille@mines.edu>
2011-09-06 20:58:37 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtTheForefront::Application.config.secret_token = '977ba1a59e72e6d08406de32fa40d95de4d4313e046a8849ce6dea54795d3a34971e4e0a1a93cf47bf6c8558b4d38c138e0e5178c96c23cc5fb488094e6f968d'
